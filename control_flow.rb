e463a32aec10de0fec62b8d121734b19081d7410
TonyHansord <59306359+TonyHansord@users.noreply.github.com>
2023-03-15 10:47:56 +1100

    completed 2 functions

--- control_flow.rb
+++ control_flow.rb
@@ -1,5 +1,10 @@
 def admin_login(username, password)
   # your code here
+  if username == "admin" || username == "Admin" && password == "12345" 
+  return "Access Granted"
+  else 
+  return "Access Denied!"
+  end  
 end
 
 def hows_the_weather(temperature)
@@ -12,5 +17,14 @@ end
 
 def calculator(operation, num1, num2)
   # your code here
+  calculation = case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else  puts "Invalid operation!"
+  end
+  
+  calculation
 end
 
